7f9d16de08fcf7e1f33fd8d98a01d7178025d53a
t0b10 <t0b10@null.void>
2025-01-22 03:18:31 -0700

    testing

--- es-app/src/QuickResume.cpp
+++ es-app/src/QuickResume.cpp
@@ -3,9 +3,7 @@
 #include "utils/FileSystemUtil.h"
 
 namespace QuickResume
-{
-    std::string logFile = "/userdata/system/qr.txt";
-    
+{    
     bool setQuickResume(std::string quickResumeCommand, std::string quickResumePath)
     {
         bool configSaved = false;
