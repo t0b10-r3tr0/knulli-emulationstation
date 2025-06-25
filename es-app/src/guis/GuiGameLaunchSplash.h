3e63aea943d66d7ca076fc1518fc53512d02a4c3
t0b10 <t0b10@null.void>
2025-06-24 18:11:07 -0600

    Fixed dependency issue.

--- es-app/src/guis/GuiGameLaunchSplash.h
+++ es-app/src/guis/GuiGameLaunchSplash.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "ExtendedGuiSettings.h"
+#include "guis/knulli/ExtendedGuiSettings.h"
 #include "components/SliderComponent.h"
 #include "components/OptionListComponent.h"
 #include "components/SwitchComponent.h"
