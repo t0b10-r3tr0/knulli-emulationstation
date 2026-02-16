96a695b6ff275f355ef2939781ff52f3309fc42c
t0b10-r3tr0 <t0b10@null.void>
2026-02-16 07:41:29 -0700

    Test for navigation prompts.

--- es-app/src/guis/GuiGameSwitcher.cpp
+++ es-app/src/guis/GuiGameSwitcher.cpp
@@ -1764,10 +1764,10 @@ std::vector<HelpPrompt> GuiGameSwitcher::getHelpPrompts()
 		return prompts;
 
 	prompts.push_back(HelpPrompt("left/right", _("NAVIGATE")));
-	prompts.push_back(HelpPrompt(BUTTON_OK, _("LAUNCH / PIN (HOLD)")));
+	prompts.push_back(HelpPrompt(BUTTON_OK, _("LAUNCHY / PIN (HOLD)")));
 	prompts.push_back(HelpPrompt(BUTTON_BACK, _("BACK")));
 	prompts.push_back(HelpPrompt("y", _("RANDOM")));
-	prompts.push_back(HelpPrompt("x", _("REMOVE (HOLD)")));
+	prompts.push_back(	("x", _("REMOVE (HOLD)")));
 	return prompts;
 }
 
